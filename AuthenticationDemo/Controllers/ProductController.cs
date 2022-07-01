33a07111254292024a311d9727b71e4116260a5c
shek12345 <shekharshinde343@gmail.com>
2022-07-01 17:01:47 +0530

    Add project files.

---
+++ AuthenticationDemo/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationDemo.Controllers
+{
+    [Authorize]
+
+    public class ProductController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
